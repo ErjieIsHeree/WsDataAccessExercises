4de91e005cf6ba9a2bf9ba058139525d511eac9c
ErjieIsHeree <erjie.xia@gmail.com>
2024-10-03 00:14:45 +0200

    All work done added

---
+++ 00_TestZone/src/methods/Menu.java
@@ -0,0 +1,28 @@
+package methods;
+
+import java.util.Scanner;
+
+public class Menu {
+	private static int selMen(int canOpc) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduzca la opción que desee:");
+		boolean optCor = false;
+		int opt = 0;
+		do {
+			try {
+				do {
+					opt = sc.nextInt();
+					sc.nextLine();
+					if (opt < 1 || opt > canOpc) {
+						System.out.println("Esta opción no existe, elija otra");
+					}
+				} while (opt < 1 || opt > canOpc);
+				optCor = true;
+			}
+			catch (Exception e) {
+				System.out.println("La opción debe ser un número de las que el menú muestre");
+			}
+		} while (!optCor);
+		return opt;
+	}
+}
