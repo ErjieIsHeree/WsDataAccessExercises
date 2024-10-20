8ea5caae5e393fc4229eaea14c82ddfe92292f22
ErjieIsHeree <erjie.xia@gmail.com>
2024-10-20 16:09:04 +0200

    Avance en XMLJava

--- 02_EsquemaXMLJava/src/gestion/ColeccionVideojuegos.java
+++ 02_EsquemaXMLJava/src/gestion/ColeccionVideojuegos.java
@@ -47,16 +47,17 @@ public class ColeccionVideojuegos {
 	}
 	
 	/**
-	 * Este metodo devuelve el contenido de todos los nodos del nodo videojuego del XML
+	 * Este metodo devuelve el los nodos hijos en forma de texto introducidos en un
+	 * ArrayList.
 	 * 
-	 * @param n
-	 * @return
+	 * @param n nodo padre del que se sacara la informacion
+	 * @return ArrayList<String> contenidod en forma de texto de los nodos hijo
 	 */
-	public static ArrayList<String> procesarVideojuego(Node n) {
+	public static ArrayList<String> procesarXMLaLista(Node n) {
 		ArrayList<String> listaVidInfo = new ArrayList<String>();
 		NodeList listaNodos = n.getChildNodes();
-		//Add atirbuto
-		listaNodos.getAttributes().item(0).getTextContent();
+		//Add atributo
+		listaVidInfo.add(n.getAttributes().item(0).getTextContent());
 		//Add elementos
 		for (int i = 0; i < listaNodos.getLength(); i++) {
 			Node nodo = listaNodos.item(i);
@@ -73,45 +74,83 @@ public class ColeccionVideojuegos {
 	 * @param doc
 	 */
 	public static void leerXML(Document doc) {
-		//Entrada
-		
+		ArrayList<String> listaVidInfo = null;
+		String salida = "";
+		Node nodo = null;
+		NodeList listaNodosVideojuego = doc.getChildNodes().item(0).getChildNodes();
 		System.out.println("\r\n"
 				+ "------------------------------------------------------\r\n"
 				+ "                 Leer XML videojuego\r\n"
-				+ "------------------------------------------------------\r\n"
-				+ "");
+				+ "------------------------------------------------------\r\n");
 		
-		String salida = null;
-		NodeList listaNodos = doc.getChildNodes();
-		
-		for (int i = 0; i < listaNodos.getLength(); i++) {
-			ArrayList<String> listaVidInfo = procesarVideojuego(listaNodos.item(i));
-			salida += "\n Videojuego: " + listaVidInfo.get(0);
-			salida += "\n Desarrollador: " + listaVidInfo.get(1);
-			salida += "\n Plataforma: " + listaVidInfo.get(2);
-			salida += "\n PEGI: " + listaVidInfo.get(4);
-			salida += "\n Anio lanzamiento: " + listaVidInfo.get(3);
-			salida += "\n PEGI: " + listaVidInfo.get(4);
-			salida += "\n Copias disponibles: " + listaVidInfo.get(5);
+		for (int i = 0; i < listaNodosVideojuego.getLength(); i++) {
+			nodo = listaNodosVideojuego.item(i);
+			if (nodo.getNodeType() == Node.ELEMENT_NODE) {
+				listaVidInfo = procesarXMLaLista(nodo);
+				salida += "\r\n------Videojuego " + listaVidInfo.get(0) + "------";
+				salida += "\r\n Desarrollador: " + listaVidInfo.get(1);
+				salida += "\r\n Plataforma: " + listaVidInfo.get(2);
+				salida += "\r\n PEGI: " + listaVidInfo.get(4);
+				salida += "\r\n Anio lanzamiento: " + listaVidInfo.get(3);
+				salida += "\r\n PEGI: " + listaVidInfo.get(4);
+				salida += "\r\n Copias disponibles: " + listaVidInfo.get(5) + "\r\n";
+			}
 		}
 		System.out.println(salida);
 	}
 	
 	/**
-	 * Método para añadir un nuevo videojuego al archivo XML. Los datos serán introducidos por el usuario y añadidos al DOM.
+	 * Metodo para aniadir un nuevo videojuego al archivo XML.
 	 * 
-	 * @param doc
-	 * @param titulo
-	 * @param desarrollador
-	 * @param plataforma
-	 * @param genero
-	 * @param anno
-	 * @param clasificacion
-	 * @param numCopias
+	 * @param doc arbol DOM en la que se encuentra el videojuego
+	 * @param titulo titulo del videojuego
+	 * @param desarrollador desarrollador del videojuego
+	 * @param plataforma plataformas disponibles de videojuego
+	 * @param genero genero del videojuego
+	 * @param anio anio de lanzamiento del videojuego
+	 * @param clasificacion clasificacion de edad del videojuego
+	 * @param numCopias cantidad de copias del videojuego
 	 */
-	public static void añadirVideojuego(Document doc, String titulo, String desarrollador, String plataforma, String genero, int anno, String clasificacion, int numCopias) {
+	public static void añadirVideojuego(Document doc, String titulo, String desarrollador, String plataforma, String genero, String anio, String clasificacion, String numCopias) {
+		Node nodoPadre = null;
+		Node nodoHijo = null;
+		Node nodo_texto = null;
 		
-		
+		try {
+			//Videojuego
+			nodoPadre = doc.createElement("videojuego");
+			nodoPadre.appendChild(doc.getFirstChild());
+			
+			//Titulo
+			((Element) nodoPadre).setAttribute("Titulo", titulo);
+			//desarrollador
+			nodoHijo = doc.createElement("Desarrollador");
+			nodo_texto = doc.createTextNode(desarrollador);
+			nodoHijo.appendChild(nodoPadre);
+			//plataforma
+			nodoHijo = doc.createElement("Plataforma");
+			nodo_texto = doc.createTextNode(plataforma);
+			nodoHijo.appendChild(nodoPadre);
+			//genero
+			nodoHijo = doc.createElement("Genero");
+			nodo_texto = doc.createTextNode(genero);
+			nodoHijo.appendChild(nodoPadre);
+			//Anio
+			nodoHijo = doc.createElement("Anio");
+			nodo_texto = doc.createTextNode(anio);
+			nodoHijo.appendChild(nodoPadre);
+			//PEGI
+			nodoHijo = doc.createElement("PEGI");
+			nodo_texto = doc.createTextNode(clasificacion);
+			nodoHijo.appendChild(nodoPadre);
+			//Titulo
+			nodoHijo = doc.createElement("Titulo");
+			nodo_texto = doc.createTextNode(numCopias);
+			nodoHijo.appendChild(nodoPadre);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
@@ -142,12 +181,21 @@ public class ColeccionVideojuegos {
 	
 	
 	/**
-	 * Método para guardar el archivo XML con las modificaciones realizadas (por ejemplo, tras añadir un nuevo videojuego).
+	 * Metodo para guardar el archivo XML con las modificaciones realizadas.
 	 * @param doc
 	 * @param archivo
 	 */
 	public static void guardarXML(Document doc, String archivo) {
-		
+		try {
+			File archivo_xml = new File("salida.xml");
+			OutputFormat format = new OutputFormat(doc);
+			format.setIndenting(true); // Indentación para mejor legibilidad 
+			// Crear el serializador XML 
+			XMLSerializer serializer = new XMLSerializer(new FileOutputStream(archivo_xml), format);
+			// Serializar el árbol DOM en el archivo XML serializer.serialize(doc);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
